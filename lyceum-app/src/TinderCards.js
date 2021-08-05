2cafa74acff694279e75b634af6eb233facc21df
Rohit Nair <nair.rohit98@gmail.com>
2021-08-05 18:39:16 -0400

    changed mongodb setup

--- lyceum-app/src/TinderCards.js
+++ lyceum-app/src/TinderCards.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import TinderCard from "react-tinder-card";
 import "./TinderCards.css";
 import axios from './axios';
